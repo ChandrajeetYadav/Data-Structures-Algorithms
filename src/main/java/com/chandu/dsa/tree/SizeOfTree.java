3455a464cbb558a85ce3f7ebcc10e7f8d137e310
Chandrajeet Yadav <i.am.chandrajeet@gmail.com>
2020-07-03 22:23:55 +0530

    Size Of Tree

---
+++ src/main/java/com/chandu/dsa/tree/SizeOfTree.java
@@ -0,0 +1,17 @@
+package com.chandu.dsa.tree;
+
+public class SizeOfTree {
+    public static void main(String[] args) {
+        BinaryTreeNode root = BinaryTreeNode.createDemoBinaryTree();
+        System.out.println("InOrder traversal of tree:");
+        TreeTraversal.inOrderTraversal(root);
+        System.out.println();
+        System.out.println("Size of Tree is: " + SizeOfTree.getSize(root));
+    }
+
+    public static int getSize(BinaryTreeNode root){
+        if (root == null)
+            return 0;
+        return getSize(root.left) + getSize(root.right) + 1;
+    }
+}
