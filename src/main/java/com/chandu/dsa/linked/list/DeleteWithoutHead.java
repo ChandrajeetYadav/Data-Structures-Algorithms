cee257aeaa784beb445a2dfc3723e821f5850e45
Chandrajeet Yadav <i.am.chandrajeet@gmail.com>
2020-07-27 22:48:34 +0530

    Delete a node in a linked list without head pointer

---
+++ src/main/java/com/chandu/dsa/linked/list/DeleteWithoutHead.java
@@ -0,0 +1,34 @@
+package com.chandu.dsa.linked.list;
+
+public class DeleteWithoutHead {
+    public static void main(String[] args) {
+        Node head = Node.createDemoLinkedList();
+        System.out.println("Original linked list: ");
+        Node.printList(head);
+        Node thirdNode = head.next.next;
+        delete(thirdNode);
+        System.out.println("Linked list after deleting third node: ");
+        Node.printList(head);
+
+        head = Node.createDemoLinkedList();
+        Node fourthNode = head.next.next.next;
+        delete(fourthNode);
+        System.out.println("Linked list after deleting fourth node: ");
+        Node.printList(head);
+
+        head = Node.createDemoLinkedList();
+        Node lastNode = head.next.next.next.next;
+        delete(lastNode);
+    }
+
+    public static void delete(Node node){
+        if(node == null || node.next==null){
+            System.out.println("Can't perform this operation as node is last node or null");
+            return;
+        }
+        Node temp = node.next;
+        node.data = temp.data;
+        node.next = temp.next;
+        temp.next = null;
+    }
+}
