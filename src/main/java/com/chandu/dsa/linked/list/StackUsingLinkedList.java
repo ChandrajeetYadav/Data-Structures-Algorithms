0dddc5ef97e7b5d72121eca90be4f4bfcc9bc83c
Chandrajeet Yadav <i.am.chandrajeet@gmail.com>
2020-07-27 23:13:22 +0530

    Implement a stack using singly linked list

---
+++ src/main/java/com/chandu/dsa/linked/list/StackUsingLinkedList.java
@@ -0,0 +1,34 @@
+package com.chandu.dsa.linked.list;
+
+public class StackUsingLinkedList {
+    public static Node top;
+
+    public static void main(String[] args) {
+        System.out.println("Initial stack: ");
+        Node.printList(top);
+        System.out.println(pop());
+        push(1);
+        push(2);
+        push(3);
+        System.out.println("Stack after pushing elements: ");
+        Node.printList(top);
+        pop();
+        System.out.println("Stack after popping elements: ");
+        Node.printList(top);
+    }
+
+    public static void push(int data){
+        Node node = new Node(data);
+        node.next = top;
+        top = node;
+    }
+
+    public static int pop(){
+        if(top == null)
+            return -1;
+        Node temp = top;
+        top = top.next;
+        temp.next = null;
+        return temp.data;
+    }
+}
