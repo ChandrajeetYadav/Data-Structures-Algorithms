2500cc6777e109b5cb6c8b9bc112fdb06758d73c
Chandrajeet <i.am.chandrajeet@gmail.com>
2020-08-22 01:03:18 +0530

    Graph node structure

---
+++ src/main/java/com/chandu/dsa/graph/GraphNode.java
@@ -0,0 +1,11 @@
+package com.chandu.dsa.graph;
+
+public class GraphNode {
+    public int data;
+    public boolean visited;
+
+    GraphNode(int data){
+        this.data = data;
+        this.visited = false;
+    }
+}
