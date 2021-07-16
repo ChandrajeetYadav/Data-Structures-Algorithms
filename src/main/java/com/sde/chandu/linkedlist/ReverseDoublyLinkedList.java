3e756968de73197be238ca0bc659bd2bf78e3905
Chandrajeet.Yadav <i.am.chandrajeet@gmail.com>
2021-07-16 12:27:16 +0530

    Reverse a Doubly Linked List

---
+++ src/main/java/com/sde/chandu/linkedlist/ReverseDoublyLinkedList.java
@@ -0,0 +1,55 @@
+package com.sde.chandu.linkedlist;
+
+import java.util.Stack;
+
+public class ReverseDoublyLinkedList {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        DoublyLinkedList head = DoublyLinkedList.createList(arr);
+        System.out.println("Original list: ");
+        head.display();
+        System.out.println("Doubly Linked list after reversing, efficient approach: ");
+        head = reverse(head);
+        head.display();
+        System.out.println();
+
+        System.out.println("Original list: ");
+        head.display();
+        System.out.println("Doubly Linked list after reversing, using stack: ");
+        head = reverseUsingStack(head);
+        head.display();
+        System.out.println();
+    }
+
+    // Time complexity : O(n)
+    // Space complexity : O(1)
+    private static DoublyLinkedList reverse(DoublyLinkedList head) {
+        DoublyLinkedList cur = head, temp = null;
+        while (cur != null) {
+            temp = cur.prev;
+            cur.prev = cur.next;
+            cur.next = temp;
+            cur = cur.prev;
+        }
+        if (temp != null)
+            head = temp.prev;
+        return head;
+    }
+
+    // Time complexity : O(n)
+    // Space complexity : O(n)
+    private static DoublyLinkedList reverseUsingStack(DoublyLinkedList head) {
+        Stack<Integer> stack = new Stack<>();
+        DoublyLinkedList temp = head;
+        while (temp != null) {
+            stack.push(temp.data);
+            temp = temp.next;
+        }
+        temp = head;
+        while (temp != null) {
+            temp.data = stack.pop();
+            temp = temp.next;
+        }
+        return head;
+    }
+}
