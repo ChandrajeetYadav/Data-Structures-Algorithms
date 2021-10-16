a96d577ea2990e6df7b5bc10d075d92178a0ac6e
Chandrajeet.Yadav <i.am.chandrajeet@gmail.com>
2021-10-17 00:05:50 +0530

    InOrder traversal of a tree

---
+++ src/main/java/com/sde/chandu/tree/BinaryTreeNode.java
@@ -0,0 +1,46 @@
+package com.sde.chandu.tree;
+
+public class BinaryTreeNode {
+    int data;
+    BinaryTreeNode left;
+    BinaryTreeNode right;
+
+    public BinaryTreeNode(int data) {
+        this.data = data;
+    }
+
+    public static BinaryTreeNode createBinaryTree() {
+        BinaryTreeNode root = new BinaryTreeNode(1);
+        root.left = new BinaryTreeNode(2);
+        root.right = new BinaryTreeNode(3);
+        root.left.left = new BinaryTreeNode(4);
+        root.left.right = new BinaryTreeNode(5);
+        root.right.left = new BinaryTreeNode(6);
+        root.right.right = new BinaryTreeNode(7);
+        return root;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public BinaryTreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(BinaryTreeNode left) {
+        this.left = left;
+    }
+
+    public BinaryTreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(BinaryTreeNode right) {
+        this.right = right;
+    }
+}
---
+++ src/main/java/com/sde/chandu/tree/InOrderTraversal.java
@@ -0,0 +1,57 @@
+package com.sde.chandu.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class InOrderTraversal {
+    public static void main(String[] args) {
+        BinaryTreeNode root = BinaryTreeNode.createBinaryTree();
+        List<Integer> listR = inOrderTraversalRecursive(root);
+        System.out.println("InOrder traversal of the tree, recursive approach: ");
+        listR.forEach(n -> System.out.print(n + " "));
+        System.out.println();
+
+        List<Integer> listI = inOrderTraversalIterativeApproach(root);
+        System.out.println("InOrder traversal of the tree, iterative approach: ");
+        listI.forEach(n -> System.out.print(n + " "));
+    }
+
+    // Time complexity : O(n)
+    // Space complexity : O(n)
+    public static List<Integer> inOrderTraversalRecursive(BinaryTreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inOrderTraversalRecursiveHelper(root, result);
+        return result;
+    }
+
+    private static void inOrderTraversalRecursiveHelper(BinaryTreeNode root, List<Integer> result) {
+        if (root == null)
+            return;
+        inOrderTraversalRecursiveHelper(root.left, result);
+        result.add(root.data);
+        inOrderTraversalRecursiveHelper(root.right, result);
+    }
+
+    // Time complexity : O(n)
+    // Space complexity : O(n)
+    public static List<Integer> inOrderTraversalIterativeApproach(BinaryTreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null)
+            return result;
+        Stack<BinaryTreeNode> stack = new Stack<>();
+        while (true) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                if (stack.isEmpty())
+                    break;
+                root = stack.pop();
+                result.add(root.data);
+                root = root.right;
+            }
+        }
+        return result;
+    }
+}
