1bc5b6450d3c5d10eb05b39074848485afcf1144
Chandrajeet.Yadav <i.am.chandrajeet@gmail.com>
2022-06-02 16:51:43 +0530

    Reverse a linked list in groups of k, without any extra space

--- src/main/java/com/sde/chandu/linkedlist/ReverseALinkedListInGroupsOfGivenSize.java
+++ src/main/java/com/sde/chandu/linkedlist/ReverseALinkedListInGroupsOfGivenSize.java
@@ -19,6 +19,12 @@ public class ReverseALinkedListInGroupsOfGivenSize {
         head.display();
         System.out.println();
 
+        System.out.println("Original list: ");
+        head.display();
+        System.out.println("List after reversing in groups of " + k + " , without extra space:");
+        head = reverseInGroupsOfKWithoutExtraSpace(head, k);
+        head.display();
+        System.out.println();
     }
 
     // Time complexity : O(n)
@@ -65,5 +71,35 @@ public class ReverseALinkedListInGroupsOfGivenSize {
         }
         prev.next = null;
         return head;
+    }
+
+    // Time complexity : O(n)
+    // Space complexity : O(1)
+    private static Node reverseInGroupsOfKWithoutExtraSpace(Node head, int k) {
+        if (head == null || head.next == null)
+            return head;
+        Node temp = head, next;
+        head = reverseHelper(head, k);
+        next = temp.next;
+        while (next != null) {
+            temp.next = reverseHelper(next, k);
+            temp = next;
+            next = next.next;
+        }
+        return head;
+    }
+
+    private static Node reverseHelper(Node head, int k) {
+        Node prev = null, next, first = head;
+        int count = 0;
+        while (count < k && head != null) {
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+            count++;
+        }
+        first.next = head;
+        return prev;
     }
 }
